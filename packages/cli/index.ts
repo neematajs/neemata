8aab065c2cca7d1c09dac542e90173332a76ef3d
Denis Ilchishin <ilchishin.denis@gmail.com>
2024-05-05 13:04:58 +0700

    feat(cli): add watch mode

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -21,6 +21,11 @@ const { values, positionals } = parseArgs({
       type: 'string',
       multiple: false,
     },
+    watch: {
+      type: 'boolean',
+      multiple: false,
+      default: false,
+    },
     timeout: {
       type: 'string',
       multiple: false,
@@ -34,7 +39,7 @@ const { values, positionals } = parseArgs({
 })
 
 const [command, ...args] = positionals
-const { env: envPaths, entry, swc, timeout, ...kwargs } = values
+const { env: envPaths, watch, entry, swc, timeout, ...kwargs } = values
 
 const shutdownTimeout =
   (typeof timeout === 'string' ? Number.parseInt(timeout) : undefined) || 1000
@@ -46,111 +51,128 @@ for (const env of envPaths as string[]) {
   }
 }
 
-const entryPath = resolve(
-  process.env.NEEMATA_ENTRY ||
-    (typeof entry === 'string' ? entry : swc ? 'index.ts' : 'index.js'),
-)
-
-let exitTimeout: Timer | undefined = undefined
-
-const exitProcess = () => {
-  if (exitTimeout) clearTimeout(exitTimeout)
-  process.exit(0)
-}
-
-const tryExit = async (cb: any) => {
-  if (exitTimeout) return
-  exitTimeout = setTimeout(exitProcess, shutdownTimeout)
-  try {
-    await cb()
-  } catch (error) {
-    logger.error(error)
-  } finally {
-    exitProcess()
-  }
-}
-
-const entryApp = await import(entryPath).then((module) => module.default)
-
-if (
-  !(
-    (NeemataServer && entryApp instanceof NeemataServer.ApplicationServer) ||
-    entryApp instanceof Application
+if (watch) {
+  const args = process.argv.slice(2).filter((arg) => arg !== '--watch')
+  Bun.spawn({
+    env: process.env,
+    cmd: [
+      'bun',
+      ...process.execArgv,
+      '--watch',
+      Bun.fileURLToPath(import.meta.url),
+      ...args,
+    ],
+    stdio: ['inherit', 'inherit', 'inherit'],
+  })
+} else {
+  const entryPath = resolve(
+    process.env.NEEMATA_ENTRY ||
+      (typeof entry === 'string' ? entry : swc ? 'index.ts' : 'index.js'),
   )
-) {
-  throw new Error(
-    'Invalid entry module. Must be an instance of Application or ApplicationServer',
-  )
-}
 
-const { logger } = entryApp
+  let exitTimeout: Timer | undefined = undefined
 
-process.on('uncaughtException', (error) => logger.error(error))
-process.on('unhandledRejection', (error) => logger.error(error))
-
-const loadApp = async (workerType: WorkerType, workerOptions = {}) => {
-  let app: Application
-
-  if (NeemataServer && entryApp instanceof NeemataServer.ApplicationServer) {
-    const { applicationPath } = entryApp.options
-
-    const options: Parameters<typeof NeemataServer.providerWorkerOptions>[0] = {
-      id: 0,
-      workerType,
-      isServer: false,
-      workerOptions,
-    }
-    NeemataServer.providerWorkerOptions(options)
-    app = await importDefault(applicationPath)
-  } else {
-    app = entryApp as Application
+  const exitProcess = () => {
+    if (exitTimeout) clearTimeout(exitTimeout)
+    process.exit(0)
   }
 
-  return app
-}
-
-const commands = {
-  async start() {
-    const terminate = () => tryExit(() => entryApp.stop())
-    process.on('SIGTERM', terminate)
-    process.on('SIGINT', terminate)
-    await entryApp.start()
-  },
-  async execute() {
-    const app = await loadApp(WorkerType.Task)
-
-    const [inputCommand, ...commandArgs] = args
-
-    let [extension, commandName] = inputCommand.split(':')
-
-    if (!commandName) {
-      commandName = extension
-      // @ts-ignore
-      extension = undefined
-    }
-
-    const terminate = () => tryExit(() => defer(() => app.stop()))
-
-    process.on('SIGTERM', terminate)
-    process.on('SIGINT', terminate)
-
-    await app.initialize()
-
-    const command = app.registry.commands
-      .get(extension ?? APP_COMMAND)
-      ?.get(commandName)
-
-    if (!command) throw new Error(`Unknown application command: ${commandName}`)
-
+  const tryExit = async (cb: any) => {
+    if (exitTimeout) return
+    exitTimeout = setTimeout(exitProcess, shutdownTimeout)
     try {
-      await command({ args: commandArgs, kwargs })
+      await cb()
+    } catch (error) {
+      logger.error(error)
     } finally {
-      terminate()
+      exitProcess()
     }
-  },
+  }
+
+  const entryApp = await import(entryPath).then((module) => module.default)
+
+  if (
+    !(
+      (NeemataServer && entryApp instanceof NeemataServer.ApplicationServer) ||
+      entryApp instanceof Application
+    )
+  ) {
+    throw new Error(
+      'Invalid entry module. Must be an instance of Application or ApplicationServer',
+    )
+  }
+
+  const { logger } = entryApp
+
+  process.on('uncaughtException', (error) => logger.error(error))
+  process.on('unhandledRejection', (error) => logger.error(error))
+
+  const loadApp = async (workerType: WorkerType, workerOptions = {}) => {
+    let app: Application
+
+    if (NeemataServer && entryApp instanceof NeemataServer.ApplicationServer) {
+      const { applicationPath } = entryApp.options
+
+      const options: Parameters<typeof NeemataServer.providerWorkerOptions>[0] =
+        {
+          id: 0,
+          workerType,
+          isServer: false,
+          workerOptions,
+        }
+      NeemataServer.providerWorkerOptions(options)
+      app = await importDefault(applicationPath)
+    } else {
+      app = entryApp as Application
+    }
+
+    return app
+  }
+
+  const commands = {
+    async start() {
+      const terminate = () => tryExit(() => entryApp.stop())
+      process.on('SIGTERM', terminate)
+      process.on('SIGINT', terminate)
+      await entryApp.start()
+    },
+    async execute() {
+      const app = await loadApp(WorkerType.Task)
+
+      const [inputCommand, ...commandArgs] = args
+
+      let [extension, commandName] = inputCommand.split(':')
+
+      if (!commandName) {
+        commandName = extension
+        // @ts-ignore
+        extension = undefined
+      }
+
+      const terminate = () => tryExit(() => defer(() => app.stop()))
+
+      process.on('SIGTERM', terminate)
+      process.on('SIGINT', terminate)
+
+      await app.initialize()
+
+      const command = app.registry.commands
+        .get(extension ?? APP_COMMAND)
+        ?.get(commandName)
+
+      if (!command)
+        throw new Error(`Unknown application command: ${commandName}`)
+
+      try {
+        await command({ args: commandArgs, kwargs })
+      } finally {
+        terminate()
+      }
+    },
+  }
+
+  if (command in commands === false)
+    throw new Error(`Unknown CLI command: ${command}`)
+
+  commands[command]()
 }
-
-if (command in commands === false)
-  throw new Error(`Unknown CLI command: ${command}`)
-
-commands[command]()
