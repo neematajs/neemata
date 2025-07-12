538c22962f0e1eb41161a2bd29bb5633d5d089ec
Denis I. <62346882+denis-ilchishin@users.noreply.github.com>
2025-07-12 15:48:01 +0700

    feat(nmtjs): add router exports (#110)

--- packages/nmtjs/src/index.ts
+++ packages/nmtjs/src/index.ts
@@ -3,11 +3,13 @@ import {
   createApplication,
   createContractNamespace,
   createContractProcedure,
+  createContractRouter,
   createFilter,
   createGuard,
   createMiddleware,
   createNamespace,
   createProcedure,
+  createRouter,
   createTask,
 } from '@nmtjs/application'
 import {
@@ -44,6 +46,8 @@ export const neemata = {
   class: createClassInjectable,
   extendClass: createExtendableClassInjectable,
   task: createTask,
+  router: createRouter,
+  contractRouter: createContractRouter,
   namespace: createNamespace,
   contractNamespace: createContractNamespace,
   procedure: createProcedure,
