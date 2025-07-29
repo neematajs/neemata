6d7f89696cf672bf70c3b7aacc3d0160a24b2036
Denis Ilchishin <ilchishin.denis@gmail.com>
2025-07-29 15:49:08 +0700

    chore(nmtjs): optimize package exports

--- packages/nmtjs/test/index.spec.ts
+++ packages/nmtjs/test/index.spec.ts
@@ -1,4 +1,11 @@
+import { describe, expect, it } from 'vitest'
+
+//
+
 import nmtjs from 'nmtjs'
+
+//
+
 import * as application from 'nmtjs/application'
 import * as cli from 'nmtjs/cli'
 import * as common from 'nmtjs/common'
@@ -11,7 +18,21 @@ import * as protocolServer from 'nmtjs/protocol/server'
 import * as server from 'nmtjs/server'
 import typeDefault, * as type from 'nmtjs/type'
 import * as wsTransport from 'nmtjs/ws-transport'
-import { describe, expect, it } from 'vitest'
+
+//
+
+import * as _application from '@nmtjs/application'
+import * as _cli from '@nmtjs/cli'
+import * as _common from '@nmtjs/common'
+import _contractDefault, * as _contract from '@nmtjs/contract'
+import * as _core from '@nmtjs/core'
+import * as _jsonFormat from '@nmtjs/json-format/server'
+import * as _protocol from '@nmtjs/protocol'
+import * as _protocolClient from '@nmtjs/protocol/client'
+import * as _protocolServer from '@nmtjs/protocol/server'
+import * as _server from '@nmtjs/server'
+import _typeDefault, * as _type from '@nmtjs/type'
+import * as _wsTransport from '@nmtjs/ws-transport'
 
 describe('nmtjs', () => {
   it('should re-export packages', () => {
@@ -30,5 +51,20 @@ describe('nmtjs', () => {
     expect(wsTransport).toBeDefined()
     expect(jsonFormat).toBeDefined()
     expect(cli).toBeDefined()
+
+    expect(_application).toStrictEqual(application)
+    expect(_cli).toStrictEqual(cli)
+    expect(_common).toStrictEqual(common)
+    expect(_contractDefault).toStrictEqual(contractDefault)
+    expect(_contract).toStrictEqual(contract)
+    expect(_core).toStrictEqual(core)
+    expect(_jsonFormat).toStrictEqual(jsonFormat)
+    expect(_protocol).toStrictEqual(protocol)
+    expect(_protocolClient).toStrictEqual(protocolClient)
+    expect(_protocolServer).toStrictEqual(protocolServer)
+    expect(_server).toStrictEqual(server)
+    expect(_type).toStrictEqual(type)
+    expect(_typeDefault).toStrictEqual(typeDefault)
+    expect(_wsTransport).toStrictEqual(wsTransport)
   })
 })
