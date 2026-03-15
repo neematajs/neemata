e8f4119db396e39d66116be49e11f81aeb264b25
Denny Il <62346882+denny-il@users.noreply.github.com>
2026-03-15 16:06:13 +0700

    refactor(type): fix temporal integration (#159)

---
+++ packages/type/src/temporal/index.ts
@@ -0,0 +1,18 @@
+import {
+  DurationType,
+  PlainDateTimeType,
+  PlainDateType,
+  PlainMonthDayType,
+  PlainTimeType,
+  PlainYearMonthType,
+  ZonedDateTimeType,
+} from '../types/temporal.ts'
+
+export const plainDate = PlainDateType.factory.bind(PlainDateType)
+export const plainDatetime = PlainDateTimeType.factory.bind(PlainDateTimeType)
+export const plainTime = PlainTimeType.factory.bind(PlainTimeType)
+export const zonedDatetime = ZonedDateTimeType.factory.bind(ZonedDateTimeType)
+export const duration = DurationType.factory.bind(DurationType)
+export const plainYearMonth =
+  PlainYearMonthType.factory.bind(PlainYearMonthType)
+export const plainMonthDay = PlainMonthDayType.factory.bind(PlainMonthDayType)
