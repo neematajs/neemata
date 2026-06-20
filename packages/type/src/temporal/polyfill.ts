868c0390a96e3af426e5c9b6aa592b8358400363
Denis Ilchishin <ilchishin.denis@gmail.com>
2026-06-20 17:16:26 +0700

    bump deps

--- packages/type/src/temporal/polyfill.ts
+++ packages/type/src/temporal/polyfill.ts
@@ -13,72 +13,40 @@ import {
 
 export const plainDate = PlainDateType.factory.bind(
   PlainDateType,
-  // @ts-expect-error
   Temporal,
-) as () => PlainDateType<
-  // @ts-expect-error
-  typeof Temporal
->
+) as () => PlainDateType<typeof Temporal>
 
 export const plainDatetime = PlainDateTimeType.factory.bind(
   PlainDateTimeType,
-  // @ts-expect-error
   Temporal,
-) as () => PlainDateTimeType<
-  // @ts-expect-error
-  typeof Temporal
->
+) as () => PlainDateTimeType<typeof Temporal>
 
 export const plainTime = PlainTimeType.factory.bind(
   PlainTimeType,
-  // @ts-expect-error
   Temporal,
-) as () => PlainTimeType<
-  // @ts-expect-error
-  typeof Temporal
->
+) as () => PlainTimeType<typeof Temporal>
 
 export const zonedDatetime = ZonedDateTimeType.factory.bind(
   ZonedDateTimeType,
-  // @ts-expect-error
   Temporal,
-) as () => ZonedDateTimeType<
-  // @ts-expect-error
-  typeof Temporal
->
+) as () => ZonedDateTimeType<typeof Temporal>
 
 export const instant = InstantType.factory.bind(
   InstantType,
-  // @ts-expect-error
   Temporal,
-) as () => InstantType<
-  // @ts-expect-error
-  typeof Temporal
->
+) as () => InstantType<typeof Temporal>
 
 export const duration = DurationType.factory.bind(
   DurationType,
-  // @ts-expect-error
   Temporal,
-) as () => DurationType<
-  // @ts-expect-error
-  typeof Temporal
->
+) as () => DurationType<typeof Temporal>
 
 export const plainYearMonth = PlainYearMonthType.factory.bind(
   PlainYearMonthType,
-  // @ts-expect-error
   Temporal,
-) as () => PlainYearMonthType<
-  // @ts-expect-error
-  typeof Temporal
->
+) as () => PlainYearMonthType<typeof Temporal>
 
 export const plainMonthDay = PlainMonthDayType.factory.bind(
   PlainMonthDayType,
-  // @ts-expect-error
   Temporal,
-) as () => PlainMonthDayType<
-  // @ts-expect-error
-  typeof Temporal
->
+) as () => PlainMonthDayType<typeof Temporal>
