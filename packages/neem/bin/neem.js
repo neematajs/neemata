fe2af580bb44de3999932d7c8722c6cfae597719
Denis Ilchishin <ilchishin.denis@gmail.com>
2026-06-21 13:40:14 +0700

    Refactor neem.js to remove development command handling

--- packages/neem/bin/neem.js
+++ packages/neem/bin/neem.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node --enable-source-maps
-import { devCommand, mainCommand } from '@nmtjs/neem/cli'
-import { runCommand, runMain } from 'citty'
+import { mainCommand } from '@nmtjs/neem/cli'
+import { runMain } from 'citty'
 
-if (process.env.NEEM_DEV_PROCESS) {
-  await runCommand(devCommand, { rawArgs: process.argv.slice(2), data: true })
-} else {
-  await runMain(mainCommand, {})
-}
+await runMain(mainCommand, {})
