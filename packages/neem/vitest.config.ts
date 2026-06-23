f5911c5e38e6847db8c6e8d8e801fe9cab694f00
Denis Ilchishin <ilchishin.denis@gmail.com>
2026-06-24 01:38:42 +0700

    test: update neem ci coverage

--- packages/neem/vitest.config.ts
+++ packages/neem/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineProject } from 'vitest/config'
 export default defineProject({
   test: {
     environment: 'node',
-    include: ['tests/**/*.spec.ts'],
+    include: ['tests/unit/**/*.spec.ts', 'tests/integration/**/*.spec.ts'],
     typecheck: { enabled: true, tsconfig: './tsconfig.json' },
   },
 })
