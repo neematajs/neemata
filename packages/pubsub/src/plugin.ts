10a23dc8881b273a9fa1b99e5f6361e0d9f499ac
Denny Il <62346882+denny-il@users.noreply.github.com>
2026-07-05 21:04:31 +0700

    fix pubsub plugin injection timing (#222)

--- packages/pubsub/src/plugin.ts
+++ packages/pubsub/src/plugin.ts
@@ -1,7 +1,10 @@
 import type { RuntimePlugin } from '@nmtjs/application'
 import type { AnyInjectable } from '@nmtjs/core'
-import { LifecycleHook } from '@nmtjs/application'
-import { provision } from '@nmtjs/core'
+import {
+  CoreInjectables,
+  createFactoryInjectable,
+  provision,
+} from '@nmtjs/core'
 
 import type { PubSubAdapter } from './adapter.ts'
 import { publish, pubsubAdapter, subscribe } from './injectables.ts'
@@ -12,18 +15,32 @@ export type PubSubPluginOptions = { adapter: AnyInjectable<PubSubAdapter> }
 export function createPubSubPlugin(
   options: PubSubPluginOptions,
 ): RuntimePlugin {
+  const manager = createFactoryInjectable({
+    dependencies: {
+      adapter: options.adapter,
+      logger: CoreInjectables.logger,
+    },
+    create: ({ adapter, logger }) => new PubSubManager({ logger, adapter }),
+  })
+
   return {
     name: 'pubsub',
-    hooks: {
-      [LifecycleHook.BeforeInitialize]: async (ctx) => {
-        const adapter = await ctx.container.resolve(options.adapter)
-        const manager = new PubSubManager({ logger: ctx.logger, adapter })
-        ctx.container.provide([
-          provision(pubsubAdapter, adapter),
-          provision(publish, manager.publish.bind(manager)),
-          provision(subscribe, manager.subscribe.bind(manager)),
-        ])
-      },
-    },
+    injections: [
+      provision(pubsubAdapter, options.adapter),
+      provision(
+        publish,
+        createFactoryInjectable({
+          dependencies: { manager },
+          create: ({ manager }) => manager.publish.bind(manager),
+        }),
+      ),
+      provision(
+        subscribe,
+        createFactoryInjectable({
+          dependencies: { manager },
+          create: ({ manager }) => manager.subscribe.bind(manager),
+        }),
+      ),
+    ],
   }
 }
